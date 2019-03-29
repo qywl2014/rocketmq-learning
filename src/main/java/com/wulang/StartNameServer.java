8492d1ad2620a1911b12d52f1fde780018dae2e9
wulang <1359208000@qq.com>
2019-03-29 10:45:59 +0800

    加注释

--- src/main/java/com/wulang/StartNameServer.java
+++ src/main/java/com/wulang/StartNameServer.java
@@ -3,6 +3,7 @@ package com.wulang;
 import org.apache.rocketmq.namesrv.NamesrvStartup;
 
 public class StartNameServer {
+    //需要设置ROCKETMQ_HOME环境变量 值为rocketmq目录
     public static void main(String[] args) {
         NamesrvStartup.main(new String[]{});
     }
